449b884dfff67c2826aead0d5d9bcec5983ec0aa
chankyu kim <chank07@naver.com>
2022-03-25 18:06:37 +0900

    nodemon 설치, 초기 코드 변경

---
+++ Readme.md
@@ -0,0 +1,6 @@
+//mongoose
+//https://runebook.dev/ko/docs/mongoose/api/model
+//https://www.codegrepper.com/code-examples/whatever/mongoose+await+on+save
+
+//exports, module.exports
+//https://dev-dain.tistory.com/55
--- server/index.js
+++ server/index.js
@@ -2,19 +2,43 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const mongoose = require('mongoose');
+const User = require('./models/User');
+const config = require('./config/key');
 
+//mongoose
 async function main() {
-  await mongoose.connect(
-    'mongodb+srv://chankyu:0818@bolierplate.ojt4g.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-  );
+  try {
+    await mongoose.connect(config.MONGO_URI);
+    console.log('mongoDB connect...');
+  } catch (error) {
+    console.log(error);
+  }
 }
+main();
 
-main() //
-  .then(console.log('MongoDB Connected...'))
-  .catch(err => console.log(err));
+//body-parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+//route
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Hello World!!!');
+});
+
+app.post('/register', async (req, res) => {
+  try {
+    const user = new User(req.body);
+    // console.log('before save');
+    // const savedUser = await user.save();
+    // console.log('savedUser', savedUser);
+    // console.log('after save');
+    await user.save();
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    return res.json({ success: false, error });
+  }
 });
 
 app.listen(port, () => {
