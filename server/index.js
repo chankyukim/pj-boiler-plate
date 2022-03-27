e46fb702305db77b966dad000b0bc49a309f6be3
chankyu kim <chank07@naver.com>
2022-03-27 14:06:15 +0900

    token을 이용한 로그인 기능(jwt)

--- server/index.js
+++ server/index.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const app = express();
-const port = 5000;
 const mongoose = require('mongoose');
 const User = require('./models/User');
-const config = require('./config/key');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+// const config = require('./config/key');
+require('dotenv').config();
 
 //mongoose
 async function main() {
   try {
-    await mongoose.connect(config.MONGO_URI);
+    await mongoose.connect(process.env.DATABASE_URL);
     console.log('mongoDB connect...');
   } catch (error) {
     console.log(error);
   }
 }
+
 main();
 
 //body-parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//cookie-parser
+app.use(cookieParser());
 
 //route
 app.get('/', (req, res) => {
@@ -43,6 +49,35 @@ app.post('/register', async (req, res) => {
   }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.post('/login', async (req, res) => {
+  try {
+    //데이터베이스에 해당하는 이메일의 사용자가 있는지 확인
+    const user = await User.findOne({ email: req.body.email }).exec();
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: '이메일에 해당하는 유저가 없습니다.',
+      });
+    }
+
+    //해당하는 이메일이 있다면 비밀번호가 맞는지 확인
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) return res.json({ loginSuccess: false, message: '비밀번호가 틀렸습니다.' });
+    //비밀번호가 맞다면 토큰 생성
+    const token = jwt.sign(user._id.toHexString(), process.env.SECRET_KEY);
+    //생성한 토큰을 해당 user의 토큰에 저장
+    user.token = token;
+    await user.save();
+    //쿠키 생성
+    res
+      .cookie('x_auth', user.token) //
+      .status(200)
+      .json({ loginSuccess: true, userId: user._id });
+  } catch (error) {
+    return res.status(400).send(error);
+  }
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
 });
