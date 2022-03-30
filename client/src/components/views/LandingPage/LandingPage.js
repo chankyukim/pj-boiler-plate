e45f678e7fc24d92ebfeb82931f8f0f5d33078e3
chankyu kim <chank07@naver.com>
2022-03-30 17:16:12 +0900

    랜딩페이지 추가

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <Container>
+      <h2>시작페이지</h2>
+    </Container>
+  );
 };
 
 export default LandingPage;
