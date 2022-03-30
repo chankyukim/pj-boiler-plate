9f9921e4a169af6aed12d3ed9e847407a136cb31
chankyu kim <chank07@naver.com>
2022-03-30 17:16:00 +0900

    라우트 설정

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signin" element={<LoginPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
