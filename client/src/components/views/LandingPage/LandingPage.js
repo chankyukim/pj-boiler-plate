b9b33cdb77f79c5ed33a4c0feecd678df88f3891
chankyu kim <chank07@naver.com>
2022-04-04 15:45:43 +0900

    로그아웃, authentication 기능

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import { signOutUser } from '../../../modules/user';
 
 const Container = styled.div`
   width: 100%;
@@ -9,9 +12,22 @@ const Container = styled.div`
   align-items: center;
 `;
 const LandingPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const onClickHandler = async () => {
+    const {
+      payload: { success },
+    } = await dispatch(signOutUser());
+
+    if (success) {
+      navigate('/signin');
+    }
+  };
   return (
     <Container>
       <h2>시작페이지</h2>
+      <button onClick={onClickHandler}>로그아웃</button>
     </Container>
   );
 };
--- client/src/modules/user.js
+++ client/src/modules/user.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 
 const LOGIN_USER = 'LOGIN_USER';
 const SIGNUP_USER = 'SIGNUP_USER';
+const SIGNOUT_USER = 'SIGNOUT_USER';
+const AUTH_USER = 'AUTH_USER';
 
 export const loginUser = dataToSubmit => async () => {
   try {
     const { data } = await axios.post('/api/users/signin', dataToSubmit);
-    // console.log('data', data);
+    console.log('signin data', data);
     return {
       type: LOGIN_USER,
       payload: data,
@@ -19,9 +21,35 @@ export const loginUser = dataToSubmit => async () => {
 export const signUpUser = dataToSubmit => async () => {
   try {
     const { data } = await axios.post('/api/users/signup', dataToSubmit);
-
+    // console.log('signup data', data);
     return {
       type: SIGNUP_USER,
+      payload: data,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signOutUser = () => async () => {
+  try {
+    const { data } = await axios.get('/api/users/signout');
+    console.log('signout data', data);
+    return {
+      type: SIGNOUT_USER,
+      payload: data,
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const auth = () => async () => {
+  try {
+    const { data } = await axios.get('/api/users/auth');
+    // console.log('authData', data);
+    return {
+      type: AUTH_USER,
       payload: data,
     };
   } catch (error) {
@@ -35,6 +63,10 @@ const user = (state = {}, action) => {
       return { ...state, loginSuccess: action.payload };
     case SIGNUP_USER:
       return { ...state, success: action.payload };
+    case SIGNOUT_USER:
+      return { ...state, success: action.payload };
+    case AUTH_USER:
+      return { ...state, userData: action.payload };
     default:
       return state;
   }
